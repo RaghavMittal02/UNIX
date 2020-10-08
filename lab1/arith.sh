109398dab457f4faeeb9c0dfb939845d45166e7c
RaghavMittal02 <53813131+RaghavMittal02@users.noreply.github.com>
2020-10-08 03:34:35 -0700

    Add files via upload

---
+++ lab1/arith.sh
@@ -0,0 +1,13 @@
+echo "enter the value of 2 operands"
+read a 
+read b
+echo "sum is"
+expr $a + $b
+echo "diff is"
+expr $a - $b
+echo "prod is"
+expr $a \* $b
+echo "divi is"
+expr $a / $b
+echo "remainder is"
+expr $a % $b
---
+++ lab1/tria.sh
@@ -0,0 +1,3 @@
+printf "\nEnter the height and base of triangle : "
+read h b
+printf "\nArea : ";echo  0.5 \* $h \* $b | bc
